0308bd3244e6c5babfc920b807c77035496f94d1
SmaranHolkar <99686725+SmaranHolkar@users.noreply.github.com>
2023-04-23 16:46:04 +0100

    Initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+export default function About() {
+  return (
+    <body>
+      <div className="about">
+        <h1 className="header">
+          <u>About</u>
+        </h1>
+        <p>
+          Techin-it is personal project of my thoughts and answers of some out
+          of the box and popular questions about technology and people.
+        </p>
+      </div>
+    </body>
+  );
+}
---
+++ src/pages/Contact.js
@@ -0,0 +1,7 @@
+export default function Contact() {
+  return (
+    <div className="contact">
+      <h1>Social Media</h1>
+    </div>
+  );
+}
